c6fc90d5c2c98e17a3733fc7e8bfe5ede92f879d
Francisco Roman <franciscoromansierra@gmail.com>
2024-01-17 22:49:23 -0500

    Removing the home link

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { ActiveLink } from '../active-link/ActiveLink';
 
 const navItems = [
-  {path: "/", text: "Home"},
   {path: "/about", text: "About"},
   {path: "/pricing", text: "Pricing"},
   {path: "/contact", text: "Contact"},
@@ -12,7 +11,7 @@ const navItems = [
 export const Navbar = () => {
   return (
     <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <Link href={navItems[0].path} className="flex items-center">
+      <Link href={"/"} className="flex items-center">
         <HomeIcon className="mr-2" />
         <span>Home</span>
       </Link>
